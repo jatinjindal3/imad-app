aa5ae3244d24b20179f304bfcfa355a68cb21c41
jatinjindal3 <30571488+jatinjindal3@users.noreply.github.com>
2017-08-05 08:37:21 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,10 +13,9 @@ app.get('/article-one', function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('/main.js', function (req, res) {
- res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-
 
 
 app.get('/article-two', function (req, res) {
